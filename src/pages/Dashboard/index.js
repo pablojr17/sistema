f17d5d1bde75f04eea6cf0ea0e474eafbd133152
pablojr17 <pablojr17@gmail.com>
2021-05-11 22:09:12 -0300

    criando tabela

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,12 +2,12 @@ import './dashboard.css';
 import { useState } from 'react';
 import Title from '../../components/Title';
 import Header from '../../components/Header';
-import { FiMessageSquare, FiPlus } from 'react-icons/fi';
+import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 
 function Dashboard() {
-  const [chamados, setChamados] = useState([]);
+  const [chamados, setChamados] = useState([1]);
 
   return (
     <div>
@@ -34,6 +34,37 @@ function Dashboard() {
               <FiPlus size={25} color="#FFf" />
           Novo chamado
         </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assuntos</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span className="badge" style={{ backgroundColor: '#5cb85c' }}>Em aberto</span>
+                  </td>
+                  <td data-label="Cadastrado">20/06/2021</td>
+                  <td data-label="Ações">
+                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                      <FiSearch color="#fff" size={17} />
+                    </button>
+                    <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                      <FiEdit2 color="#fff" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </>
         )}
       </div>
